62125bec88808df818e945ab8f31b229462b1318
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-03-11 17:39:33 +0900

    수업

--- SUBQUERY 문제.sql
+++ SUBQUERY 문제.sql
@@ -66,9 +66,13 @@ WHERE HIRE_DATE IN (SELECT MIN(HIRE_DATE)
 ORDER BY HIRE_DATE;
 --부서별로 그룹을 묶을 때 퇴사한 직원을 서브쿼리에서 제외해야함
 --왜? 부서별로 가장 빠른 입사자 구했을 때 D8 부서는 이태림임(이태림은 퇴사자)
--- 문제점 : 부서별로 가장 빠른 입사자 구해놓고, 메인쿼리에서 퇴사자 제외해버리면
+--문제점 : 부서별로 가장 빠른 입사자 구해놓고, 메인쿼리에서 퇴사자 제외해버리면
 --D8 부서는 퇴사자인 이태림이 가장 빠른 입사자이기 때문에
 --전제 부서중 D8 부서가 아예 제외되어 버린다.
+--부서별 가장 빠른 입사자 구할때(서브쿼리) 퇴사한 직원을 뺀 상태에서 그룹으로 묶으면
+-- D8부서의 가장 빠른 입사자는 이태림 제외 후 전형돈이 된다.
+
+
 
 --7번
 SELECT EMP_ID,EMP_NAME,JOB_NAME,나이,보너스포함연봉  --컬럼
@@ -80,8 +84,8 @@ FROM EMPLOYEE
 NATURAL JOIN JOB)
 
 WHERE (나이,JOB_CODE) IN (SELECT MIN(TRUNC(MONTHS_BETWEEN(TRUNC(SYSDATE), (19 || SUBSTR(EMP_NO,1,6))) /12)),JOB_CODE
-              FROM EMPLOYEE
-              GROUP BY JOB_CODE)
+                         FROM EMPLOYEE
+                         GROUP BY JOB_CODE)
                
 ORDER BY 나이 DESC;  --TO_CHAR(컬럼값,'패턴') --> FM작성하면 사이에 공백 없어짐
                      -- EX)   TO_CHAR( (SALARY * (NVL(BONUS,0) + 1)) *12, '999,999,999') = ￦ 123,456,789
@@ -90,6 +94,32 @@ ORDER BY 나이 DESC;  --TO_CHAR(컬럼값,'패턴') --> FM작성하면 사이�
 
 --SELECT SYSDATE FROM DUAL; --2024-03-08 10:00:12.000
 --SELECT TRUNC(SYSDATE) FROM DUAL; --2024-03-08 00:00:00.000  ->시간 자름
+
+
+
+--7번 풀이
+
+--서브쿼리
+SELECT MAX(EMP_NO) FROM EMPLOYEE
+GROUP BY JOB_CODE;
+
+--메인쿼리
+SELECT EMP_ID,EMP_NAME,JOB_NAME,
+FLOOR(MONTHS_BETWEEN(SYSDATE ,TO_DATE(SUBSTR(EMP_NO,1,6), 'RRMMDD') ) /12) "나이", --FLOOR : 소수점 첫쨰자리에서 버림
+     TO_CHAR(SALARY * (1+ NVL(BONUS,0)) *12, 'L999,999,999') "보너스 포함 연봉" -- L :그 나라 화폐단위로 찍힘
+FROM EMPLOYEE
+NATURAL JOIN JOB
+WHERE EMP_NO IN (SELECT MAX(EMP_NO) FROM EMPLOYEE
+                 GROUP BY JOB_CODE) --직급별 나이가 가장 어린 직원이기때문에 JOB_CODE로 묶음
+ORDER BY "나이" DESC;
+
+--TO_DATE(SUBSTR(EMP_NO,1,6), 'RRMMDD') EX ) 1997/12/23 이렇게 뜸
+--TO_DATE(SUBSTR(EMP_NO,1,6), 'YYMMDD') EX ) 2010/12/23 이렇게 뜸
+
+
+
+
+
 
 
 
