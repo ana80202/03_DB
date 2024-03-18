8f83c464a405f33f3bff7544e5cf18576ba2233b
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-03-18 16:43:29 +0900

    수업

--- 00_사용자_계정_생성.sql
+++ 00_사용자_계정_생성.sql
@@ -6,16 +6,16 @@
  * */
 
 --11G 버전 이전의 문법을 사용 가능하도록 함
-ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;  --이거 꼭 수행해야 밑에거 수행됨! 잊지말기
 -- CTRL + ENTER : 선택한 SQL 수행
 
 --사용자 계정 생성
-CREATE USER cdy_sample IDENTIFIED BY sample1234 ;
+CREATE USER board_project IDENTIFIED BY board1234 ;
 
 -- 사용자 계정에 권한 부여
-GRANT RESOURCE, CONNECT TO  sample1234 ;
+GRANT RESOURCE, CONNECT TO  board_project ;
 
 --객체가 생성될 수 있는 공간 할당량 지정
-ALTER USER sample1234 DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+ALTER USER  board_project DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
 
 
